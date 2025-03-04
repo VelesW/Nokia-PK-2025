f9fb373b873b1c3506d00be5a96f94cb4a5cbe15
VelesW <michalwilk296@gmail.com>
2025-03-04 15:56:31 +0100

    Initial attach

--- src/UE/Application/States/NotConnectedState.cpp
+++ src/UE/Application/States/NotConnectedState.cpp
@@ -1,4 +1,5 @@
 #include "NotConnectedState.hpp"
+#include "ConnectingState.hpp"
 
 namespace ue
 {
@@ -6,7 +7,15 @@ namespace ue
 NotConnectedState::NotConnectedState(Context &context)
     : BaseState(context, "NotConnectedState")
 {
+    
+}
 
+void NotConnectedState::handleSib(common::BtsId btsId) {
+    using namespace std::chrono_literals;
+    context.bts.sendAttachRequest(btsId);
+    context.timer.startTimer(500ms);
+    // context.user.showConnecting();
+    context.setState<ConnectingState>();
 }
 
 }
--- src/UE/Tests/Application/ApplicationTestSuite.cpp
+++ src/UE/Tests/Application/ApplicationTestSuite.cpp
@@ -12,11 +12,13 @@
 namespace ue
 {
 using namespace ::testing;
+using namespace std::chrono_literals;
 
 class ApplicationTestSuite : public Test
 {
 protected:
     const common::PhoneNumber PHONE_NUMBER{112};
+    const common::BtsId BTS_ID{22};
     NiceMock<common::ILoggerMock> loggerMock;
     StrictMock<IBtsPortMock> btsPortMock;
     StrictMock<IUserPortMock> userPortMock;
@@ -29,11 +31,29 @@ protected:
                                 timerPortMock};
 };
 
-struct ApplicationNotConnectedTestSuite : ApplicationTestSuite
-{};
+struct ApplicationNotConnectedTestSuite : ApplicationTestSuite {
+    void shallHandleSibMessage() {
+        EXPECT_CALL(btsPortMock, sendAttachRequest(BTS_ID));
+        EXPECT_CALL(timerPortMock,  startTimer(500ms));
+        EXPECT_CALL(userPortMock,   showConnecting());
 
-TEST_F(ApplicationNotConnectedTestSuite, todo)
-{
+        objectUnderTest.handleSib(BTS_ID);
+    }
+};
+
+TEST_F(ApplicationNotConnectedTestSuite, shallHandleSibMessage) {
+    shallHandleSibMessage();
+}
+
+struct ApplicationConnectingTestSuite : ApplicationNotConnectedTestSuite {
+    ApplicationConnectingTestSuite() {
+        shallHandleSibMessage();
+    }
+};
+
+TEST_F(ApplicationConnectingTestSuite, shallHandleAttachAccept) {
+    EXPECT_CALL(userPortMock, showConnected());
+    objectUnderTest.handleAttachAccept();
 }
 
 }
